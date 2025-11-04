e16fd723afc2e4348963ca9969cb326aaf8f31ca
Shanu Malik <shanu.malik@cardekho.com>
2025-11-04 14:38:44 +0530

    init api

---
+++ assignment_api/src/main/java/com/example/assignmentportal/AssignmentportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignmentportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentportalApplication.class, args);
+	}
+
+}
---
+++ assignment_api/src/test/java/com/example/assignmentportal/AssignmentportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
